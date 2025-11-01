0dd6f604f57ef851501870f1ccee11bca07a8e27
pro-grammer-SD <geniussantu1983@gmail.com>
2025-11-01 15:28:45 +0530

    up

--- main.py
+++ main.py
@@ -1,20 +1,61 @@
 import os
-import shutil
-import psutil
-import torch
+os.environ["HY3DGEN_NO_MESH_CLEANUP"] = "1"
+
 import streamlit as st
+import torch, tempfile
+from hy3dgen.shapegen.pipelines import Hunyuan3DDiTFlowMatchingPipeline
+import trimesh
+from streamlit_stl import stl_from_file
 
 st.set_page_config(page_title="Hunyuan3D Generator", layout="centered")
 st.title("🌀 Hunyuan3D Web UI")
 
-total_ram = round(psutil.virtual_memory().total / (1024 ** 3), 2)
-used_ram = round(psutil.virtual_memory().used / (1024 ** 3), 2)
-disk_total = round(shutil.disk_usage("/").total / (1024 ** 3), 2)
-disk_free = round(shutil.disk_usage("/").free / (1024 ** 3), 2)
-cpu_cores = os.cpu_count()
+@st.cache_resource
+def load_pipe():
+    pipe = Hunyuan3DDiTFlowMatchingPipeline.from_pretrained(
+        "tencent/Hunyuan3D-2mini",
+        subfolder="hunyuan3d-dit-v2-mini-turbo",
+        use_safetensors=True
+    ).to("cpu")
+    pipe.to(dtype=torch.float16)
+    torch.set_num_threads(8)
+    torch.set_num_interop_threads(8)
+    return pipe
 
-st.sidebar.header("🧠 System Info")
-st.sidebar.markdown(f"**CPU Cores:** {cpu_cores}")
-st.sidebar.markdown(f"**RAM:** {used_ram} GB / {total_ram} GB")
-st.sidebar.markdown(f"**Storage Free:** {disk_free} GB / {disk_total} GB")
-st.sidebar.markdown(f"**Torch Device:** {'CUDA' if torch.cuda.is_available() else 'CPU'}")
+pipe = load_pipe()
+
+file = st.file_uploader("Upload an image", type=["png", "jpg", "jpeg"])
+
+if file:
+    st.image(file, caption="Preview", use_container_width=True)
+    if st.button("🧠 Generate 3D Model"):
+        with st.spinner("Generating 3D model... please wait ⏳"):
+            with tempfile.NamedTemporaryFile(delete=False, suffix=".png") as tmp:
+                tmp.write(file.read())
+                mesh = pipe(
+                    image=tmp.name,
+                    num_inference_steps=10,
+                    octree_resolution=256,
+                    output_type="trimesh"
+                )[0]
+                out_path = "model.stl"
+                mesh.export(out_path)
+
+            st.success("✅ 3D model generated!")
+            with open(out_path, "rb") as f:
+                st.download_button("⬇️ Download model.stl", f, file_name="model.stl")
+
+            st.subheader("🧩 3D Model Preview")
+            stl_from_file(
+                file_path=out_path,
+                color="#FF9900",
+                material="material",
+                auto_rotate=True,
+                opacity=1,
+                shininess=100,
+                cam_v_angle=60,
+                cam_h_angle=-90,
+                height=500,
+                max_view_distance=1000,
+            )
+            
